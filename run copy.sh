3a3e54e4eafe42ce8cc2329fb026cd58d085322a
YYYYXL1004 <2498378289@qq.com>
2025-11-19 19:55:16 +0800

    在作者数据集上尝试

--- run copy.sh
+++ run copy.sh
@@ -8,6 +8,4 @@ accelerate launch --config_file accelerate_config_ddp.yaml main.py \
     --cycle=2 \
     --eval_step=2 \
     --rec_kl_loss=0.0001 \
-    --rec_dec_cl_loss=0.0003 \
     --id_kl_loss=0.0001 \
-    --id_dec_cl_loss=0.0003
