749273db7200a8fb6c931a50eafb44fdb843b6f1
YYYYXL1004 <2498378289@qq.com>
2025-12-08 15:49:00 +0800

    GRPO V1最终提交

--- run_grpo.sh
+++ run_grpo.sh
@@ -7,7 +7,7 @@ SFT_CKPT=./myckpt/${DATASET}/Dec-04-2025_00-55-5925d6/11.pt
 accelerate launch --config_file accelerate_config_ddp.yaml train_grpo.py \
     --config ./config/${DATASET}.yaml \
     --sft_ckpt ${SFT_CKPT} \
-    --grpo_group_size=4 \
+    --grpo_group_size=2 \
     --grpo_kl_coeff=0.05 \
     --grpo_temperature=1.0 \
     --grpo_reward_alpha=0.5 \
